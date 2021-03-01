cf9a0fd86641a8160682d70bf24dc7d6c54c24b1
Tyler Shockey <tyler@blackdogdigi.com>
2021-02-28 22:57:01 -0700

    home hero

--- wp-content/themes/elevate/components/content/views/hero-phone.php
+++ wp-content/themes/elevate/components/content/views/hero-phone.php
@@ -5,6 +5,16 @@
     //$DATA->subheadline
     //$DATA->phone_img
 ?>
-<section class="hero-phone bkgd-img" style="background-image:url(<?php echo $DATA->bkgd_img['url']; ?>);">
-
+<section class="hero-phone position-relative bkgd-img" style="background-image:url(<?php echo $DATA->bkgd_img['url']; ?>);">
+    <div class="headline-section pt-4 pb-5 px-3 p-md-5 text-center text-md-left">
+        <h1 class="text-white mb-3"><?php echo($DATA->headline)? $DATA->headline : get_the_title(); ?></h1>
+        <?php if (!empty($DATA->subheadline)): ?>
+            <h6 class="text-white line-1"><?php echo $DATA->subheadline; ?></h6>
+        <?php endif; ?>
+    </div>
+    <?php if (!empty($DATA->phone_img)): ?>
+        <div class="phone-section">
+            <img src="<?php echo $DATA->phone_img['url']; ?>" alt="<?php echo $DATA->phone_img['alt']; ?>">
+        </div>
+    <?php endif; ?>
 </section>
