8ad87532182b6b5ef36c7bdc53ce775bd386f59a
Tyler Shockey <tyler@blackdogdigi.com>
2021-03-03 09:49:41 -0700

    partners section

--- wp-content/themes/elevate/components/content/views/adapters-partnerships.php
+++ wp-content/themes/elevate/components/content/views/adapters-partnerships.php
@@ -5,7 +5,7 @@
     //         $item->img
     // $DATA->partner_content
     // $DATA->partners
-    //         $item->featured_image
+    //         $item->img
     // $DATA->see_all_link
 ?>
 <section id="flex-section-<?php echo $DATA->row_index; ?>" class="adapters-partnerships bg-bkgd-gray py-5 py-md-6">
@@ -20,7 +20,7 @@
                     <?php echo $DATA->adapter_content; ?>
                 </div>
             </div>
-            <div class="col-12 col-lg-8 pt-3 pb-3 pt-md-6 pb-md-6 pt-lg-10 pb-lg-9">
+            <div class="col-12 col-lg-8 pt-3 pb-3 pt-md-6 pb-md-6 pt-lg-10 pb-lg-10">
                 <div class="wheel-container">
                     <div class="adapter-wheel">
                         <div class="wheel">
@@ -55,5 +55,38 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                <hr class="full-white">
+            </div>
+            <?php if (!empty($DATA->partner_content)): ?>
+                <div class="col-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8 my-5 my-md-6">
+                    <div class="content text-center">
+                        <?php echo $DATA->partner_content; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+        <div class="row partners-row">
+            <?php 
+            if (!empty($DATA->partners)){ 
+                foreach ($DATA->partners as $p) {
+            ?>
+            <div class="col-6 col-md-4 col-lg-3 mb-4 mb-md-5">
+                <div class="one-logo">
+                    <img src="<?php echo $p->img['url']; ?>" alt="<?php echo $p->img['alt']; ?>">
+                </div>
+            </div>
+            <?php 
+                }
+            } ?>
+        </div>
+        <?php if (!empty($DATA->see_all_link)): ?>
+            <div class="row mb-5 mb-md-7">
+                <div class="col-12 text-center">
+                    <a href="<?php echo $DATA->see_all_link['url']; ?>" target="<?php echo $DATA->see_all_link['target'] ? $DATA->see_all_link['target'] : '_self'; ?>" class="btn-outline txt-black mt-4"><?php echo $DATA->see_all_link['title']; ?></a>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
