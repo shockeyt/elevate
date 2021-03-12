0330a45ad779ffbd4180aaf53ad5cb0b502e3e7b
Tyler Shockey <tyler@blackdogdigi.com>
2021-03-11 22:45:50 -0700

    smooth scroll anchor links

--- wp-content/themes/elevate/assets/js/scripts.js
+++ wp-content/themes/elevate/assets/js/scripts.js
@@ -13,6 +13,13 @@
 
 	});
 
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+    });
 
 	$(window).load(function(){
 
